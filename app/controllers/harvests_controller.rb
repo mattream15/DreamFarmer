343575ad060c75c96cd62301dfe8836ac70bf39e
Matthew Ream <matthewtimothyream@gmail.com>
2019-12-06 23:51:55 -0600

    Developing random number generator

--- app/controllers/harvests_controller.rb
+++ app/controllers/harvests_controller.rb
@@ -1,6 +1,7 @@
 class HarvestsController < ApplicationController
 
     def create
+        @harvest.cannabis_plant.weight_of_flowers.random_number(5..8)
         #random number generator in the create method
         #use harvested flag on the plant itself for that plant that is being saved
         #check to see if plant that is going to be harvested hasn't been harvested yet
